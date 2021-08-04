cc35dc13910c87cb577aa55c21e62c956c239e23
khubbard96 <36139573+khubbard96@users.noreply.github.com>
2021-08-03 21:52:33 -0500

    Create push.sh

---
+++ scripts/push.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git checkout -b deploy
+  git add build
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git push --quiet --set-upstream deploy
+}
+
+setup_git
+commit_website_files
+upload_files
